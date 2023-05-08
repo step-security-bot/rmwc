1d44e9dab4da002be486b5aca33304a8c44cccfa
Andreas Lüdemann <andreaslydemann@gmail.com>
2023-05-08 08:48:01 +0100

    Add new readme files

--- website/src/theme/ReactLiveScope/index.js
+++ website/src/theme/ReactLiveScope/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {Avatar, AvatarGroup, AvatarCount, } from '@rmwc/avatar';
+import { TextField } from '@rmwc/textfield';
+import { Chip } from '@rmwc/chip';
+import { Badge, BadgeAnchor } from '@rmwc/badge';
+import { IconButton } from '@rmwc/icon-button';
 import { Button } from '@rmwc/button';
 import { CircularProgress } from '@rmwc/circular-progress';
 import { DocWrapper} from '@site/src/components'
@@ -6,7 +11,15 @@ import { DocWrapper} from '@site/src/components'
 // Add react-live imports you need here
 const ReactLiveScope = {
   React,
+  Avatar,
+  AvatarGroup, 
+  AvatarCount,
+  Badge,
+  BadgeAnchor,
+  Chip,
+  TextField,
   Button,
+  IconButton,
   CircularProgress,
   DocWrapper,
   ...React,
