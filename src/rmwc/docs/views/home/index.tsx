9420cf33616b1549cc2adea07cde82929a0e3888
Andreas Lüdemann <andreaslydemann@gmail.com>
2023-05-09 13:44:00 +0200

    Use home global css

--- src/rmwc/docs/views/home/index.tsx
+++ src/rmwc/docs/views/home/index.tsx
@@ -8,7 +8,6 @@ import { Theme } from '@rmwc/theme';
 import { Icon } from '@rmwc/icon';
 
 import styles from './home.module.css';
-import './home.global.css';
 import { Button } from '@rmwc/button';
 
 import { galleryContent } from '../../common/menu-content';
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Home } from 'rmwc/docs/views/home/index';
+import 'rmwc/docs/views/home/home.global.css';
 import './globals.css';
 
 export default function Page() {
