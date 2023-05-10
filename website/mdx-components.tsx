bfd60268f58bed97d9972075faa70a891965ff28
Andreas Lüdemann <andreaslydemann@gmail.com>
2023-05-10 21:05:01 +0200

    Update dependencies

--- website/mdx-components.tsx
+++ website/mdx-components.tsx
@@ -9,7 +9,10 @@ import type { MDXComponents } from 'mdx/types';
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
-    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    // h1: ({ children }) => <h1 style={{ fontSize: '100px' }}>{children}</h1>,
+    /*code: ({ children, className }) => (
+      <CodeBlock className={className}>{children}</CodeBlock>
+    ),*/
     ...components
   };
 }
